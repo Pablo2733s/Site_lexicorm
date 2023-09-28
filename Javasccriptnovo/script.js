626bb1e9d6bd2b1e5cc2c4d59499801584638c3d
Pablo <129318813+Pablo2733s@users.noreply.github.com>
2023-09-28 15:53:05 -0300

    javascript

---
+++ Javasccriptnovo/script.js
@@ -0,0 +1,29 @@
+// var btn = document.querySelector("button");
+
+// function random(number) {
+//   return Math.floor(Math.random() * (number + 1));
+// }
+
+// btn.onclick = function () {
+//   var rndCol =
+//     "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+//   document.body.style.backgroundColor = rndCol;
+// };
+// var btn = document.querySelector("button");
+
+// function bgChange() {
+//   var rndCol =
+//     "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+//   document.body.style.backgroundColor = rndCol;
+// }
+
+// btn.addEventListener("click", bgChange);
+
+// function bgChange(e) {
+//     var rndCol =
+//       "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+//     e.target.style.backgroundColor = rndCol;
+//     console.log(e);
+//   }
+  
+//   btn.addEventListener("click", bgChange);
